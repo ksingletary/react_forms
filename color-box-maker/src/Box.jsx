003a9c4144f110bdd0ffa6beca7cea33055f13f7
Keith Kodes <code.keith@Keiths-MBP.lan>
2024-03-01 13:37:24 -0500

    react forms exercises

---
+++ color-box-maker/src/Box.jsx
@@ -0,0 +1,17 @@
+const Box = ({bgcolor, width, height, remove}) => {
+
+    const handleRemove = () =>{
+      remove()
+    } 
+  
+    return (
+      <>
+      <div style={{width:width, height:height, backgroundColor:bgcolor}} >
+        <button onClick={handleRemove}>X</button>
+      </div>
+        </>
+    )
+  }
+  
+  export default Box
+   
